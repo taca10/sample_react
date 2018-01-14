b6aaf7e2e21bc683cd85481bb789a91814acfc23
taca10 <taka.ur.0505@gmail.com>
2018-01-14 19:12:50 +0900

    section30

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -4,13 +4,23 @@ import PropTypes from 'prop-types';
 
 const HotelsRow = ({ hotel }) => (
   <tr>
+    <td><img src={hotel.thumbUrl} alt={hotel.name} /></td>
     <td><a href={hotel.url} target="_blank">{hotel.name} </a>{hotel.name}</td>
+    <td>{hotel.price}</td>
+    <td>{hotel.reviewCount}</td>
+    <td>{hotel.distance}</td>
   </tr>
 );
 
 HotelsRow.propTypes = {
   hotel: PropTypes.shape({
     name: PropTypes.string,
+    url: PropTypes.string,
+    thumbUrl: PropTypes.string,
+    price: PropTypes.string,
+    reviewAverage: PropTypes.number,
+    reviewCount: PropTypes.number,
+    distance: PropTypes.number,
   }).isRequired,
 };
 
